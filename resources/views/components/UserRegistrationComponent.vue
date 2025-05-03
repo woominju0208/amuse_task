39cb0b9d4ae6ce558a6cfadb99fd32662e1a8a89
woominju0208 <alswn5033@gmail.com>
2025-05-03 17:34:37 +0900

    회원가입 수정

--- resources/views/components/UserRegistrationComponent.vue
+++ resources/views/components/UserRegistrationComponent.vue
@@ -2,9 +2,9 @@
     <div class="form-box">
         <h2 class="form=title">회원가입</h2>
         <input type="text" name="account" placeholder="아이디" autocomplete="off">
+        <input type="text" name="name" placeholder="이름" autocomplete="off">
         <input type="password" name="password" placeholder="비밀번호" autocomplete="off">
         <input type="password" name="password_chk" placeholder="비밀번호 확인" autocomplete="off">
-        <input type="text" name="name" placeholder="이름" autocomplete="off">
         
         <hr>
         <button class="btn btn-submit btn-bg-black">회원가입</button>
